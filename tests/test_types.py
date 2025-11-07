94a0ef25f918e0bb1891743b397dc0d131b4bd6f
Walnut Geek <wg@walnutgeek.com>
2025-11-07 00:04:14 -0800

    KNOWN_TYPES tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,7 +1,11 @@
 # Placeholder for an empty build.
+from datetime import UTC, date, datetime
+from typing import Any
+
+import pytest
 from pydantic import Field
 
-from lythonic.types import JsonBase
+from lythonic.types import KNOWN_TYPES, JsonBase, KnownType
 
 
 class A(JsonBase):
@@ -33,3 +37,44 @@ def test_json_base():
     }
     # jb = JsonBase(type_gref=GlobalRef(JsonBase))
     # assert jb.type_gref == GlobalRef(JsonBase)
+
+
+@pytest.mark.debug
+def test_known_types():
+    def do_roundtrip_by_type(raw: Any, map_to: str | None = None, fail: bool = False) -> None:
+        ktype: KnownType = KNOWN_TYPES.resolve_type(type(raw))  # pyright: ignore
+        mapped_str = ktype.string.map_to(raw)
+        mapped_json = ktype.json.map_to(raw)
+        mapped_db = ktype.db.map_to(raw)
+        actual = f"{mapped_str=} -> {mapped_json=} -> {mapped_db=}"
+        if map_to is not None:
+            assert map_to == actual, f"Expected {map_to} but got {actual}"
+        else:
+            print(f'do_roundtrip_by_type({raw!r}, "{actual}")')
+        assert ktype.string.map_from(mapped_str) == raw, (
+            f"Failed to roundtrip {raw} by string. ktype: {ktype}"
+        )
+        assert ktype.json.map_from(mapped_json) == raw, (
+            f"Failed to roundtrip {raw} by json. ktype: {ktype}"
+        )
+        assert ktype.db.map_from(mapped_db) == raw, (
+            f"Failed to roundtrip {raw} by db. ktype: {ktype}"
+        )
+        if fail:
+            raise AssertionError
+
+    do_roundtrip_by_type(
+        date(2025, 11, 6),
+        "mapped_str='2025-11-06' -> mapped_json='2025-11-06' -> mapped_db='2025-11-06'",
+    )
+    do_roundtrip_by_type(
+        datetime(2025, 11, 7, 7, 58, 51, 107831, tzinfo=UTC),
+        "mapped_str='2025-11-07T07:58:51.107831+00:00' -> mapped_json='2025-11-07T07:58:51.107831+00:00' -> mapped_db='2025-11-07T07:58:51.107831+00:00'",
+    )
+    do_roundtrip_by_type(1, "mapped_str='1' -> mapped_json=1 -> mapped_db=1")
+    do_roundtrip_by_type(True, "mapped_str='True' -> mapped_json=True -> mapped_db=True")
+    do_roundtrip_by_type(5.4, "mapped_str='5.4' -> mapped_json=5.4 -> mapped_db=5.4")
+    do_roundtrip_by_type("hello", "mapped_str='hello' -> mapped_json='hello' -> mapped_db='hello'")
+    do_roundtrip_by_type(
+        b"hello", "mapped_str='aGVsbG8=' -> mapped_json='aGVsbG8=' -> mapped_db=b'hello'"
+    )
